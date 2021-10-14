cb29c4f5d2a15830eb420136bb6851bdd8e9ebd9
gtraiano <gtraiano@gmail.com>
2021-10-14 15:02:10 +0300

    added id property to returned JSON in `toJSON()`

--- backend/src/models/Diagnosis.ts
+++ backend/src/models/Diagnosis.ts
@@ -20,7 +20,7 @@ const DiagnosisSchema = new Schema<Diagnosis>({
 
 DiagnosisSchema.set('toJSON', {
     transform: (_document, returnedObject) => {
-      //returnedObject.id = returnedObject._id.toString();
+      returnedObject.id = returnedObject._id.toString();
       delete returnedObject._id;
       delete returnedObject.__v;
     }
