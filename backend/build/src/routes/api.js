e5894ee826c803a841f7144943c6b23272a1d061
gtraiano <gtraiano@gmail.com>
2022-01-13 14:06:13 +0200

    untrack build folder

--- backend/build/src/routes/api.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const api = (0, express_1.Router)();
-api.get('/ping', (_req, res) => {
-    res.send('pong');
-});
-exports.default = api;
