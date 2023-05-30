5cb4471e1e8f14d41fcbbe0ccd8956ac7cba6882
gtraiano <gtraiano@gmail.com>
2023-05-30 22:16:02 +0300

    ignore ssl issues for domain icdcodelookup.com (domain's certificate is expired at the time of commit)

--- backend/src/services/icdcodelookup.ts
+++ backend/src/services/icdcodelookup.ts
@@ -1,11 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import axios from 'axios';
+import { Agent } from 'https';
 
 const apiUrl = 'https://s.icdcodelookup.com/lookUp/search';
+// ignore ssl errors
+const httpsAgent = new Agent({
+    rejectUnauthorized: false
+});
 
 const searchTerms = async (terms: string): Promise<unknown> => {
-    const { data } = await axios.post(apiUrl, { terms });
+    const { data } = await axios.post(apiUrl, { terms }, { httpsAgent });
     return data;
 };
 
